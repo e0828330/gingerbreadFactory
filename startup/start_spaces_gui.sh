3eafb888298ae561c89501d8efb34272b146da9d
Adel Gadllah <adel.gadllah@gmail.com>
2013-12-13 21:30:17 +0100

    Add spaces startup scripts

---
+++ startup/start_spaces_gui.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp ../gingerbreadFactory/target/gingerbreadFactory-0.0.1-SNAPSHOT-jar-with-dependencies.jar factory.gui.GuiMain
---
+++ startup/start_spaces_server.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -Xmx2g -cp ../gingerbreadFactory/target/gingerbreadFactory-0.0.1-SNAPSHOT-jar-with-dependencies.jar factory.spacesImpl.Server
