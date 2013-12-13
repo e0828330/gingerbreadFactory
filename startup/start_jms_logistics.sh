9dec952bcf9411882bb30ab1546ae1f68302c840
Adel Gadllah <adel.gadllah@gmail.com>
2013-12-13 21:33:23 +0100

    Add jms startup scripts

---
+++ startup/start_jms_logistics.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+if [ "$#" -ne 1 ]; then
+    echo "Please supply an ID"
+fi
+java -cp ../gingerbreadFactory/target/gingerbreadFactory-0.0.1-SNAPSHOT-jar-with-dependencies.jar factory.jmsImpl.logistics.JMSLogistics $1
