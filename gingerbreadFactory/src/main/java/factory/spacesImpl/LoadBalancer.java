d78405db51b42ef77294c07b4906f7c35314187c
Adel Gadllah <adel.gadllah@gmail.com>
2014-01-16 23:35:56 +0100

    spaces: Load balancer fixes

--- gingerbreadFactory/src/main/java/factory/spacesImpl/LoadBalancer.java
+++ gingerbreadFactory/src/main/java/factory/spacesImpl/LoadBalancer.java
@@ -83,62 +83,64 @@ public class LoadBalancer implements Runnable {
 		
 		try {
 			ContainerReference container = capi.lookupContainer("ordersLB", new URI(loadBalancerURL), MzsConstants.RequestTimeout.INFINITE, null);
-			ArrayList<Order> orders = capi.take(container, FifoCoordinator.newSelector(1), MzsConstants.RequestTimeout.INFINITE, null);
-			Order currentOrder = orders.get(0);
-			
-			System.out.println("Got order from factory: " + currentOrder.getFactoryId());
-			
-			double avgOpenPackages = 0.;
-			
-			int minFactoryId = 0;
-			int minPackageCount = Integer.MAX_VALUE;
-			
-			HashMap<Integer, Integer> openPackages = new HashMap<Integer, Integer>();
-			
-			for (Integer factory : factories.keySet()) {
-				int numOpen = 0;
-				/* First get all open packages */
-				Order tpl = new Order();
-				tpl.setState(Order.State.OPEN);
-				ArrayList<Order> tmpList = capi.read(factories.get(factory), LindaCoordinator.newSelector(tpl, MzsConstants.Selecting.COUNT_ALL), MzsConstants.RequestTimeout.INFINITE, null);
-				for (Order tmp : tmpList) {
-					numOpen += tmp.getPackages();
+			while (true) {
+				ArrayList<Order> orders = capi.take(container, FifoCoordinator.newSelector(1), MzsConstants.RequestTimeout.INFINITE, null);
+				Order currentOrder = orders.get(0);
+				
+				System.out.println("Got order from factory: " + currentOrder.getFactoryId());
+				
+				double avgOpenPackages = 0.;
+				
+				int minFactoryId = 0;
+				int minPackageCount = Integer.MAX_VALUE;
+				
+				HashMap<Integer, Integer> openPackages = new HashMap<Integer, Integer>();
+				
+				for (Integer factory : factories.keySet()) {
+					int numOpen = 0;
+					/* First get all open packages */
+					Order tpl = new Order();
+					tpl.setState(Order.State.OPEN);
+					ArrayList<Order> tmpList = capi.read(factories.get(factory), LindaCoordinator.newSelector(tpl, MzsConstants.Selecting.COUNT_ALL), MzsConstants.RequestTimeout.INFINITE, null);
+					for (Order tmp : tmpList) {
+						numOpen += tmp.getPackages();
+					}
+					
+					/* Do the same for the in progess orders */
+					tpl.setState(Order.State.IN_PROGRESS);
+					tmpList = capi.read(factories.get(factory), LindaCoordinator.newSelector(tpl, MzsConstants.Selecting.COUNT_ALL), MzsConstants.RequestTimeout.INFINITE, null);
+					for (Order tmp : tmpList) {
+						numOpen += (tmp.getPackages() - tmp.getDonePackages());
+					}
+					
+					/* Store result and prepare for avg calculation */
+					openPackages.put(factory, numOpen);
+					avgOpenPackages += (double) numOpen;
+					
+					/* Store the factory that has the minimum load */
+					if (numOpen < minPackageCount) {
+						minPackageCount = numOpen;
+						minFactoryId = factory;
+					}
 				}
 				
-				/* Do the same for the in progess orders */
-				tpl.setState(Order.State.IN_PROGRESS);
-				tmpList = capi.read(factories.get(factory), LindaCoordinator.newSelector(tpl, MzsConstants.Selecting.COUNT_ALL), MzsConstants.RequestTimeout.INFINITE, null);
-				for (Order tmp : tmpList) {
-					numOpen += (tmp.getPackages() - tmp.getDonePackages());
-				}
+				avgOpenPackages = avgOpenPackages / (double) factories.size();
+	
+				System.out.println("Averange open packages per factory: " + avgOpenPackages);
 				
-				/* Store result and prepare for avg calculation */
-				openPackages.put(factory, numOpen);
-				avgOpenPackages += (double) numOpen;
-				
-				/* Store the factory that has the minimum load */
-				if (numOpen < minPackageCount) {
-					minPackageCount = numOpen;
-					minFactoryId = factory;
+				/* If we are 25% above average move otherwise stay in the same */
+				if (openPackages.get(currentOrder.getFactoryId()) + currentOrder.getPackages() > avgOpenPackages * 1.25 || avgOpenPackages < 0.01) {
+					ContainerReference targetFactory = factories.get(currentOrder.getFactoryId());
+					currentOrder.setState(Order.State.MOVED);
+					currentOrder.setFactoryId(minFactoryId);
+					capi.write(new Entry(currentOrder), targetFactory, MzsConstants.RequestTimeout.INFINITE, null);
+					currentOrder.setState(Order.State.OPEN);
+					capi.write(new Entry(currentOrder),  factories.get(minFactoryId), MzsConstants.RequestTimeout.INFINITE, null);
 				}
-			}
-			
-			avgOpenPackages = avgOpenPackages / (double) factories.size();
-
-			System.out.println("Averange open packages per factory: " + avgOpenPackages);
-			
-			/* If we are 25% above average move otherwise stay in the same */
-			if (openPackages.get(currentOrder.getFactoryId()) + currentOrder.getPackages() > avgOpenPackages * 1.25) {
-				ContainerReference targetFactory = factories.get(currentOrder.getFactoryId());
-				currentOrder.setState(Order.State.MOVED);
-				currentOrder.setFactoryId(minFactoryId);
-				capi.write(new Entry(currentOrder), targetFactory, MzsConstants.RequestTimeout.INFINITE, null);
-				currentOrder.setState(Order.State.OPEN);
-				capi.write(new Entry(currentOrder),  factories.get(minFactoryId), MzsConstants.RequestTimeout.INFINITE, null);
-			}
-			else {
-				ContainerReference targetFactory = factories.get(currentOrder.getFactoryId());
-				capi.write(new Entry(currentOrder), targetFactory, MzsConstants.RequestTimeout.INFINITE, null);
+				else {
+					ContainerReference targetFactory = factories.get(currentOrder.getFactoryId());
+					capi.write(new Entry(currentOrder), targetFactory, MzsConstants.RequestTimeout.INFINITE, null);
+				}
 			}
 			
 		} catch (MzsCoreException e) {
